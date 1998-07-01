03cc0a7f45247baad98f29254f5d64f3ee33aac4
escoffon <escoffon>
1998-07-01 18:32:09 +0000

    Initial revision

---
+++ tk/win/rmd.bat
@@ -0,0 +1,20 @@
+@echo off
+rem SCCS: %Z% $Id$ 
+
+if not exist %1 goto end
+
+if %OS% == Windows_NT goto winnt
+
+echo Add support for Win 95 please
+goto end
+
+goto success
+
+:winnt
+rmdir %1 /s /q
+if errorlevel 1 goto end
+
+:success
+echo deleted directory %1
+
+:end
