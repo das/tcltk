1599f2652192981695b5ea11028b16e0fcea1d57
ericm <ericm>
1999-11-17 22:13:02 +0000

    * tests/listbox.test: Added tests for ListboxUpdateHScrollbar.
    * generic/tkListbox.c: Changed some old static buffers to base
    size on TCL_DOUBLLE_SPACE instead of (completely) hardcoding the size.

--- tk/generic/tkListbox.c
+++ tk/generic/tkListbox.c
@@ -2667,7 +2667,7 @@ static void
 ListboxUpdateVScrollbar(listPtr)
     register Listbox *listPtr;		/* Information about widget. */
 {
-    char string[100];
+    char string[TCL_DOUBLE_SPACE * 2];
     double first, last;
     int result;
     Tcl_Interp *interp;
@@ -2729,7 +2729,7 @@ static void
 ListboxUpdateHScrollbar(listPtr)
     register Listbox *listPtr;		/* Information about widget. */
 {
-    char string[60];
+    char string[TCL_DOUBLE_SPACE * 2];
     int result, windowWidth;
     double first, last;
     Tcl_Interp *interp;
