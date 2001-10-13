aaf35c5e86b37a0085e7574e79a638da96b3fdde
hobbs <hobbs>
2001-10-13 01:22:39 +0000

    Win64 pointer fixes

--- tk/win/tkWinDialog.c
+++ tk/win/tkWinDialog.c
@@ -1415,7 +1415,7 @@ MakeFilter(interp, string, dsPtr)
 	 * Since we may only add asterisks (*) to the filter, we need at most
 	 * twice the size of the string to format the filter
 	 */
-	filterStr = ckalloc(strlen(string) * 3);
+	filterStr = ckalloc((unsigned int) strlen(string) * 3);
 
 	for (filterPtr = flist.filters, p = filterStr; filterPtr;
 	        filterPtr = filterPtr->next) {
@@ -1473,7 +1473,7 @@ MakeFilter(interp, string, dsPtr)
 	*p = '\0';
     }
 
-    Tcl_DStringAppend(dsPtr, filterStr, p - filterStr);
+    Tcl_DStringAppend(dsPtr, filterStr, (int) (p - filterStr));
     ckfree((char *) filterStr);
 
     TkFreeFileFilters(&flist);
@@ -2096,14 +2096,7 @@ SetTkDialog(ClientData clientData)
     ThreadSpecificData *tsdPtr = (ThreadSpecificData *) 
             Tcl_GetThreadData(&dataKey, sizeof(ThreadSpecificData));
     char buf[32];
-    HWND hwnd;
 
-    hwnd = (HWND) clientData;
-
-#ifdef _WIN64
-    sprintf(buf, "0x%16x", hwnd);
-#else
-    sprintf(buf, "0x%08x", hwnd);
-#endif
+    sprintf(buf, "0x%p", (HWND) clientData);
     Tcl_SetVar(tsdPtr->debugInterp, "tk_dialog", buf, TCL_GLOBAL_ONLY);
 }
