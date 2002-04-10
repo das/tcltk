83fa573bb0e87e5ca1b07b87d7f8b464ddc3a753
hobbs <hobbs>
2002-04-10 19:56:50 +0000

    fixed copyright notice

--- tk/tests/all.tcl
+++ tk/tests/all.tcl
@@ -5,7 +5,9 @@
 # in this directory.
 #
 # Copyright (c) 1998-1999 by Scriptics Corporation.
-# All rights reserved.
+#
+# See the file "license.terms" for information on usage and redistribution
+# of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 # 
 # RCS: @(#) $Id$
 
