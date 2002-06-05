eb6e73cae95c868ccb67167bf41800a07b52905a
a_kovalenko <a_kovalenko>
2002-06-05 22:11:09 +0000

    * unix/tkUnixFont.c (Tk_DrawChars): Don't assume that one char is always one byte, and that required subfont for the last character in any string is the same as for the previous character [Bug #559435] [Patch #559437]

--- tk/unix/tkUnixFont.c
+++ tk/unix/tkUnixFont.c
@@ -1159,6 +1159,7 @@ Tk_DrawChars(display, drawable, gc, tkfont, source, numBytes, x, y)
     Tcl_UniChar ch;
     FontFamily *familyPtr;
     int rx, ry, width, height, border_width, depth;
+    int do_width;
     Drawable root;
 
     fontPtr = (UnixFont *) tkfont;
@@ -1178,18 +1179,20 @@ Tk_DrawChars(display, drawable, gc, tkfont, source, numBytes, x, y)
 
     end = source + numBytes;
     needWidth = fontPtr->font.fa.underline + fontPtr->font.fa.overstrike;
-    for (p = source; p < end; ) {
-	next = p + Tcl_UtfToUniChar(p, &ch);
-	thisSubFontPtr = FindSubFontForChar(fontPtr, ch);
+    for (p = source; p <= end; ) {
+	if (p < end) {
+	    next = p + Tcl_UtfToUniChar(p, &ch);
+	    thisSubFontPtr = FindSubFontForChar(fontPtr, ch);
+	} else {
+	    next = p + 1;
+	    thisSubFontPtr = lastSubFontPtr;
+	}
 	if ((thisSubFontPtr != lastSubFontPtr)
-		|| (p == end-1) || (p-source > 200)) {
-	    if (p == end-1) {
-		p++;
-	    }
+		|| (p == end) || (p-source > 200)) {
 	    if (p > source) {
-	        int do_width = (needWidth || (p != end-1)) ? 1 : 0;
-
+	        do_width = (needWidth || (p != end)) ? 1 : 0;
 		familyPtr = lastSubFontPtr->familyPtr;
+
 		Tcl_UtfToExternalDString(familyPtr->encoding, source,
 			p - source, &runString);
 		if (familyPtr->isTwoByteFont) {
@@ -1238,6 +1241,9 @@ Tk_DrawChars(display, drawable, gc, tkfont, source, numBytes, x, y)
 		(unsigned) (x - xStart), (unsigned) fontPtr->barHeight);
     }
 }
+
+
+
 
 /*
  *-------------------------------------------------------------------------
