3211454c31f113b7ffbf848bd1bb87d79c907ac7
dgp <dgp>
2002-06-13 15:31:38 +0000

    * tests/cursor.test:  corrected error after cursor-2.2.         * tests/defs.tcl: Added enhancements to Tk's fake version of         tcltest required by recent cursor.test changes.

--- tk/tests/defs.tcl
+++ tk/tests/defs.tcl
@@ -784,7 +784,10 @@ proc ::tcltest::normalizeMsg {msg} {
 # cleanupTests was called, add it to the $filesMade list, so it will
 # be removed by the next call to cleanupTests.
 #
-proc ::tcltest::makeFile {contents name} {
+proc ::tcltest::makeFile {contents name {directory {}}} {
+    if {[string length $directory]} {
+	set name [file join $directory $name]
+    }
     set fd [open $name w]
     fconfigure $fd -translation lf
     if {[string index $contents [expr {[string length $contents] - 1}]] == "\n"} {
@@ -819,6 +822,7 @@ proc ::tcltest::makeDirectory {name} {
     if {[lsearch -exact $::tcltest::filesMade $fullName] == -1} {
 	lappend ::tcltest::filesMade $fullName
     }
+    return $fullName
 }
 
 proc ::tcltest::removeDirectory {name} {
