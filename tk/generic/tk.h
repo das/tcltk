2d60059b734b77f80fd1e3863537f6499e5d237b
dgp <dgp>
2002-07-14 17:29:57 +0000

    * Bumped HEAD to version 8.4b2 in order to distinguish it from 	  the 8.4b1 release.  Also extended LOCALES to cover all message           catalogs.

--- tk/generic/tk.h
+++ tk/generic/tk.h
@@ -48,10 +48,10 @@ extern "C" {
 #define TK_MAJOR_VERSION   8
 #define TK_MINOR_VERSION   4
 #define TK_RELEASE_LEVEL   TCL_BETA_RELEASE
-#define TK_RELEASE_SERIAL  1
+#define TK_RELEASE_SERIAL  2
 
 #define TK_VERSION	"8.4"
-#define TK_PATCH_LEVEL	"8.4b1"
+#define TK_PATCH_LEVEL	"8.4b2"
 
 /*
  * The following definitions set up the proper options for Macintosh
