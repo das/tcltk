5485516f55a317177f0ae4fe178547fe7d0ca51c
patthoyts <patthoyts>
2003-07-28 21:14:11 +0000

    * library/tk.tcl: Apply fix for bug #778840 for window placement 	when using multiple desktops on windows.

--- tk/library/tk.tcl
+++ tk/library/tk.tcl
@@ -100,6 +100,10 @@ proc ::tk::PlaceWindow {w {place ""} {anchor ""}} {
 	set y [expr {([winfo screenheight $w]-[winfo reqheight $w])/2}]
 	set checkBounds 0
     }
+    if {[tk windowingsystem eq "win32"]} {
+        # Bug 533519: win32 multiple desktops may produce negative geometry.
+        set checkBounds 0
+    }
     if {$checkBounds} {
 	if {$x < 0} {
 	    set x 0
