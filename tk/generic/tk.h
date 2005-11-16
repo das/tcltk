6cc7a9f2f377a91d28cb015dca60806d0caa95b7
dgp <dgp>
2005-11-16 22:07:36 +0000

    * README:               Bump version number to 8.4.12         * generic/tk.h:         * macosx/Wish.pbproj/project.pbxproc:         * unix/configure.in:         * unix/tk.spec:         * win/configure.in:
            * unix/configure:       autoconf
            * win/configure:

--- tk/generic/tk.h
+++ tk/generic/tk.h
@@ -50,10 +50,10 @@ extern "C" {
 #define TK_MAJOR_VERSION   8
 #define TK_MINOR_VERSION   4
 #define TK_RELEASE_LEVEL   TCL_FINAL_RELEASE
-#define TK_RELEASE_SERIAL  11
+#define TK_RELEASE_SERIAL  12
 
 #define TK_VERSION	"8.4"
-#define TK_PATCH_LEVEL	"8.4.11"
+#define TK_PATCH_LEVEL	"8.4.12"
 
 /*
  * The following definitions set up the proper options for Macintosh
