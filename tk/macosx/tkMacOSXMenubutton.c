cd2f8e80ce21b066c516ce454e7c964dc3892a25
das <das>
2005-11-27 06:47:33 +0000

    typo in last commit

--- tk/macosx/tkMacOSXMenubutton.c
+++ tk/macosx/tkMacOSXMenubutton.c
@@ -22,7 +22,7 @@
 #if !defined(MAC_OS_X_VERSION_10_3) || \
         (MAC_OS_X_VERSION_MAX_ALLOWED < MAC_OS_X_VERSION_10_3)
     /* Define constants only available on Mac OS X 10.3 or later */
-    #define kMenuAttrDoNotUseUserCommandKeys = (1 << 7)
+    #define kMenuAttrDoNotUseUserCommandKeys (1 << 7)
 #endif
 
 #define kShadowOffset   (3)     /* amount to offset shadow from frame */
