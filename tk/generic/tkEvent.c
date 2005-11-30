ccbd62f11d7a9c4bb0ea2917fcb0c45bd4e77712
hobbs <hobbs>
2005-11-30 21:13:12 +0000

    * generic/tkEvent.c: fix handling of ClientMessage handlers. 	[Bug 1289565 983920]

--- tk/generic/tkEvent.c
+++ tk/generic/tkEvent.c
@@ -983,11 +983,11 @@ Tk_HandleEvent(eventPtr)
 		    Tk_InternAtom((Tk_Window) winPtr, "WM_PROTOCOLS")) {
 		TkWmProtocolEventProc(winPtr, eventPtr);
 	    } else {
-		/* 
+		/*
 		 * Finally, invoke any ClientMessage event handlers.
 		 */
 
-		for (genPrevPtr = NULL, genericPtr = tsdPtr->cmList;  
+		for (genPrevPtr = NULL, genericPtr = tsdPtr->cmList;
 		     genericPtr != NULL; ) {
 		    if (genericPtr->deleteFlag) {
 			if (!tsdPtr->handlersActive) {
@@ -1006,7 +1006,7 @@ Tk_HandleEvent(eventPtr)
 				genPrevPtr->nextPtr = tmpPtr;
 			    }
 			    if (tmpPtr == NULL) {
-				tsdPtr->lastGenericPtr = genPrevPtr;
+				tsdPtr->lastCmPtr = genPrevPtr;
 			    }
 			    (void) ckfree((char *) genericPtr);
 			    genericPtr = tmpPtr;
