3bbbf5a2e8d9d68d14791ca45a74564eaec4d02a
hobbs <hobbs>
2006-12-04 20:13:21 +0000

    * generic/tkListbox.c (ConfigureListboxItem): ListboxWorldChanged 	not needed - just call EventuallyRedrawRange. [Bug 1608046] (rezic)

--- tk/generic/tkListbox.c
+++ tk/generic/tkListbox.c
@@ -384,7 +384,7 @@ static int		ConfigureListbox(Tcl_Interp *interp, Listbox *listPtr,
 			    int objc, Tcl_Obj *CONST objv[], int flags);
 static int		ConfigureListboxItem(Tcl_Interp *interp,
 			    Listbox *listPtr, ItemAttr *attrs, int objc,
-			    Tcl_Obj *CONST objv[]);
+			    Tcl_Obj *CONST objv[], int index);
 static int		ListboxDeleteSubCmd(Listbox *listPtr,
 			    int first, int last);
 static void		DestroyListbox(char *memPtr);
@@ -931,7 +931,7 @@ ListboxWidgetObjCmd(clientData, interp, objc, objv)
 	    }
 	} else {
 	    result = ConfigureListboxItem(interp, listPtr, attrPtr,
-		    objc-3, objv+3);
+		    objc-3, objv+3, index);
 	}
 	break;
     }
@@ -1694,13 +1694,14 @@ ConfigureListbox(interp, listPtr, objc, objv, flags)
  */
 
 static int
-ConfigureListboxItem(interp, listPtr, attrs, objc, objv)
+ConfigureListboxItem(interp, listPtr, attrs, objc, objv, index)
     Tcl_Interp *interp;		/* Used for error reporting. */
     register Listbox *listPtr;	/* Information about widget; may or may not
 				 * already have values for some fields. */
     ItemAttr *attrs;		/* Information about the item to configure */
     int objc;			/* Number of valid entries in argv. */
     Tcl_Obj *CONST objv[];	/* Arguments. */
+    int index;			/* Index of the listbox item being configure */
 {
     Tk_SavedOptions savedOptions;
 
@@ -1711,7 +1712,11 @@ ConfigureListboxItem(interp, listPtr, attrs, objc, objv)
 	return TCL_ERROR;
     }
     Tk_FreeSavedOptions(&savedOptions);
-    ListboxWorldChanged((ClientData) listPtr);
+    /*
+     * Redraw this index - ListboxWorldChanged would need to be called
+     * if item attributes were checked in the "world".
+     */
+    EventuallyRedrawRange(listPtr, index, index);
     return TCL_OK;
 }
 
