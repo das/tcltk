dc4387d34825ecb5b410cf44a40ac551c50db2d7
hobbs <hobbs>
2007-04-13 00:21:47 +0000

    * library/ttk/panedwindow.tcl (ttk::panedwindow::Press): handle 	Press triggering outside sash element boundaries.

--- tk/library/ttk/panedwindow.tcl
+++ tk/library/ttk/panedwindow.tcl
@@ -34,7 +34,7 @@ proc ttk::panedwindow::Press {w x y} {
     variable State
 
     lassign [$w identify $x $y] sash element
-    if {![info exists sash]} {
+    if {![info exists sash] || $sash eq ""} {
     	set State(pressed) 0
 	return
     }
