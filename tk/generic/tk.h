4e2dd33b16c5fdd6902d6612039a9799df9dcb90
dgp <dgp>
2007-05-30 14:06:20 +0000

    * README:               Bump version number to 8.4.16         * generic/tk.h:         * unix/configure.in:         * unix/tk.spec:         * win/configure.in:
            * unix/configure:       autoconf-2.13
            * win/configure:

--- tk/generic/tk.h
+++ tk/generic/tk.h
@@ -48,10 +48,10 @@ extern "C" {
 #define TK_MAJOR_VERSION   8
 #define TK_MINOR_VERSION   4
 #define TK_RELEASE_LEVEL   TCL_FINAL_RELEASE
-#define TK_RELEASE_SERIAL  15
+#define TK_RELEASE_SERIAL  16
 
 #define TK_VERSION	"8.4"
-#define TK_PATCH_LEVEL	"8.4.15"
+#define TK_PATCH_LEVEL	"8.4.16"
 
 #ifndef _TCL
 #   include <tcl.h>
