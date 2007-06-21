c9b06675c353c1bfc3c07ff87b8f11d7c91d0c63
hobbs <hobbs>
2007-06-21 00:29:16 +0000

    * library/ttk/ttk.tcl: should require Tk before pseudo-providing 	tile 0.8.0.

--- tk/library/ttk/ttk.tcl
+++ tk/library/ttk/ttk.tcl
@@ -45,7 +45,7 @@ proc ttk::deprecated'warning {old new} {
 ### Backward-compatibility.
 #
 
-package ifneeded tile 0.8.0 { package provide tile 0.8.0 }
+package ifneeded tile 0.8.0 { package require Tk ; package provide tile 0.8.0 }
 
 # ttk::panedwindow used to be named ttk::paned.  Keep the alias for now.
 #
