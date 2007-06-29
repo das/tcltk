5c52999fc95a85a22113f03ad7e1ef3e5906de8b
das <das>
2007-06-29 03:20:58 +0000

    * xlib/ximage.c:		bitmaps created from the static .bmp 					arrays always have LSBFirst bit order.

--- tk/xlib/ximage.c
+++ tk/xlib/ximage.c
@@ -54,6 +54,8 @@ XCreateBitmapFromData(display, d, data, width, height)
     }
     ximage = XCreateImage(display, NULL, 1, XYBitmap, 0, (char*) data, width,
 	    height, 8, (width + 7) / 8);
+    ximage->bitmap_bit_order = LSBFirst;
+    _XInitImageFuncPtrs(ximage);
     TkPutImage(NULL, 0, display, pix, gc, ximage, 0, 0, 0, 0, width, height);
     ximage->data = NULL;
     XDestroyImage(ximage);
