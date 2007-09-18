ff174418e65bd8bcb8d0cca6600f501c98c3a745
patthoyts <patthoyts>
2007-09-18 10:39:50 +0000

    Made ttk/all.tcl be the same as tk's all.tcl and make use of file normalize

--- tk/tests/all.tcl
+++ tk/tests/all.tcl
@@ -15,7 +15,7 @@ package require Tcl 8.5
 package require tcltest 2.2
 package require Tk ;# This is the Tk test suite; fail early if no Tk!
 tcltest::configure {*}$argv
-tcltest::configure -testdir [file join [pwd] [file dirname [info script]]]
+tcltest::configure -testdir [file normalize [file dirname [info script]]]
 tcltest::configure -loadfile \
 	[file join [tcltest::testsDirectory] constraints.tcl]
 tcltest::configure -singleproc 1
