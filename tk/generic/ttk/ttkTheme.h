17ecbb8dab91b0723575a9671b1478013da2a069
jenglish <jenglish>
2007-10-15 15:03:40 +0000

    generic/ttk/ttkTreeview.c:  Store pointer to column table entry instead of column index in columnNames hash table.
    This avoids the need for the evil PTR2INT and INT2PTR macros,
    and simplifies things a bit.

--- tk/generic/ttk/ttkTheme.h
+++ tk/generic/ttk/ttkTheme.h
@@ -19,28 +19,6 @@ extern "C" {
 #   endif
 #endif
 
-#ifdef HAVE_SYS_TYPES_H
-#    include <sys/types.h>
-#endif
-#if !defined(INT2PTR) && !defined(PTR2INT)
-#   if defined(HAVE_INTPTR_T) || defined(intptr_t)
-#	define INT2PTR(p) ((void*)(intptr_t)(p))
-#	define PTR2INT(p) ((int)(intptr_t)(p))
-#   else
-#	define INT2PTR(p) ((void*)(p))
-#	define PTR2INT(p) ((int)(p))
-#   endif
-#endif
-#if !defined(UINT2PTR) && !defined(PTR2UINT)
-#   if defined(HAVE_UINTPTR_T) || defined(uintptr_t)
-#	define UINT2PTR(p) ((void*)(uintptr_t)(p))
-#	define PTR2UINT(p) ((unsigned int)(uintptr_t)(p))
-#   else
-#	define UINT2PTR(p) ((void*)(p))
-#	define PTR2UINT(p) ((unsigned int)(p))
-#   endif
-#endif
-
 #define TTKAPI MODULE_SCOPE
 
 /* Ttk syncs to the Tk version & patchlevel */
