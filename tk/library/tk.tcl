26744f3d93b9d94a19cac14ef1b632b4840cfc18
patthoyts <patthoyts>
2008-05-11 00:47:22 +0000

    Support for ttk widgets in AmpWidget

--- tk/library/tk.tcl
+++ tk/library/tk.tcl
@@ -355,7 +355,7 @@ if {![llength [info command tk_chooseDirectory]]} {
 # Define the set of common virtual events.
 #----------------------------------------------------------------------
 
-switch -- [tk windowingsystem] {
+switch -exact -- [tk windowingsystem] {
     "x11" {
 	event add <<Cut>> <Control-Key-x> <Key-F20> 
 	event add <<Copy>> <Control-Key-c> <Key-F16>
@@ -510,7 +510,7 @@ proc ::tk::AmpWidget {class path args} {
 	}
     }
     set result [$class $path {*}$options]
-    if {$class eq "button"} {
+    if {[string match "*button" $class]} {
 	bind $path <<AltUnderlined>> [list $path invoke]
     }
     return $result
@@ -539,8 +539,8 @@ proc ::tk::AmpMenuArgs {widget add type args} {
 #
 proc ::tk::FindAltKeyTarget {path char} {
     switch -- [winfo class $path] {
-	Button -
-	Label {
+	Button - Label - 
+        TButton - TLabel - TCheckbutton {
 	    if {[string equal -nocase $char \
 		  [string index [$path cget -text] [$path cget -underline]]]} {
 		return $path
