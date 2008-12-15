4c0bef3de3231ecb2baf18dc4169304f0a795c81
das <das>
2008-12-15 17:51:31 +0000

    fix 'make checkstubs' failure: make TtkEntry_Init MODULE_SCOPE

--- tk/generic/ttk/ttkEntry.c
+++ tk/generic/ttk/ttkEntry.c
@@ -2016,7 +2016,7 @@ TTK_END_LAYOUT
 /*------------------------------------------------------------------------
  * +++ Initialization.
  */
-
+MODULE_SCOPE
 void TtkEntry_Init(Tcl_Interp *interp)
 {
     Ttk_Theme themePtr =  Ttk_GetDefaultTheme(interp);
