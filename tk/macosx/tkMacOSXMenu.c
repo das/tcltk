f230a1124133257cf6e086ceb6827734979c07e8
nijtmans <nijtmans>
2009-01-28 20:47:49 +0000

    - eliminate some unnessary type casts - some internal const decorations - spacing

--- tk/macosx/tkMacOSXMenu.c
+++ tk/macosx/tkMacOSXMenu.c
@@ -537,7 +537,7 @@ int
 	Tcl_AppendResult(interp, "No more menus can be allocated.", NULL);
 	return TCL_ERROR;
     }
-    Tcl_SetHashValue(commandEntryPtr, (char *) menuPtr);
+    Tcl_SetHashValue(commandEntryPtr, menuPtr);
     *menuIDPtr = returnID;
     return TCL_OK;
 }
@@ -2650,7 +2650,7 @@ DrawMenuEntryAccelerator(
 		    CFRelease(cfStr);
 		}
 	    } else {
-		Tk_DrawChars(menuPtr->display, d, gc, tkfont, accel + 
+		Tk_DrawChars(menuPtr->display, d, gc, tkfont, accel +
 			geometryPtr->accelTextStart, mePtr->accelLength -
 			geometryPtr->accelTextStart, leftEdge, baseline);
 	    }
