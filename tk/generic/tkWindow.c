2c7b9e4451f08823d180c1f3db2a916a2e689d9d
Daniel A. Steffen <das@users.sourceforge.net>
2009-03-20 07:53:14 +0100

    <rdar://problem/6586050>: native file dialogs implementation
    - add new -command option to [tk_getOpenFile], [tk_getSaveFile] and
      [tk_chooseDirectory] to enable window-modal operation:
      if -command is present (may have empty value) along with -parent, sheet
      dialog is window-modal for parent, and the file dialog command has empty
      return value; once sheet dialog is closed sucessfully (not with Cancel),
      value of -command with chosen file/directory appended is evaluated.

--- tk/generic/tkWindow.c
+++ tk/generic/tkWindow.c
@@ -187,11 +187,9 @@ static TkCmd commands[] = {
 
 #if defined(__WIN32__) || defined(MAC_OSX_TK)
     {"tk_chooseColor",	NULL,		Tk_ChooseColorObjCmd,	NULL, 0, 1},
-#ifdef MAC_OSX_TK_TODO
     {"tk_chooseDirectory", NULL,	Tk_ChooseDirectoryObjCmd,NULL, 0,1},
     {"tk_getOpenFile",	NULL,		Tk_GetOpenFileObjCmd,	NULL, 0, 1},
     {"tk_getSaveFile",	NULL,		Tk_GetSaveFileObjCmd,	NULL, 0, 1},
-#endif
     {"tk_messageBox",	NULL,		Tk_MessageBoxObjCmd,	NULL, 0, 1},
 #endif
 
