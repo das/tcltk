8e2f84c4e557f950a95700166271c892cf8c6118
Daniel A. Steffen <das@users.sourceforge.net>
2009-03-20 08:15:51 +0100

    <rdar://problem/6586050>: native tk_messageBox implementation
    - add new -command option to [tk_messageBox] to enable window-modal
    operation: if -command is present (may have empty value) along with
    -parent, sheet alert is window-modal for parent, and [tk_messageBox] has
    empty return value; once sheet alert completes, value of -command with
    chosen button name appended is evaluated.
    (cherry picked from commit b21e2d0c4461e64c0f54edb312649e00d9d576fb)

--- tk/generic/tkWindow.c
+++ tk/generic/tkWindow.c
@@ -184,11 +184,13 @@ static TkCmd commands[] = {
      * these commands differently (via the script library).
      */
 
-#if defined(__WIN32__) || defined(MAC_OSX_TK_TODO)
+#if defined(__WIN32__) || defined(MAC_OSX_TK)
+#ifdef MAC_OSX_TK_TODO
     {"tk_chooseColor",	NULL,			Tk_ChooseColorObjCmd,	0, 1},
     {"tk_chooseDirectory", NULL,		Tk_ChooseDirectoryObjCmd,0,1},
     {"tk_getOpenFile",	NULL,			Tk_GetOpenFileObjCmd,	0, 1},
     {"tk_getSaveFile",	NULL,			Tk_GetSaveFileObjCmd,	0, 1},
+#endif
     {"tk_messageBox",	NULL,			Tk_MessageBoxObjCmd,	0, 1},
 #endif
 
