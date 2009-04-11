efca8a7a4beae7e33adb762b9dffe8712c9cc8fa
dkf <dkf>
2009-04-11 12:49:49 +0000

    Oops!

--- tk/library/tk.tcl
+++ tk/library/tk.tcl
@@ -369,7 +369,7 @@ switch -exact -- [tk windowingsystem] {
 	event add <<Undo>> <Control-Key-z> <Control-Lock-Key-Z>
 	event add <<Redo>> <Control-Key-Z> <Control-Lock-Key-z>
 	event add <<ContextMenu>> <Button-3>
-	if {$tk_platform(os) eq "Darwin"} {
+	if {$tcl_platform(os) eq "Darwin"} {
 	    event add <<ContextMenu>> <Button-2>
 	}
 
