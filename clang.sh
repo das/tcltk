9b14fafa71e3208e13480592cf735ee85ab2ff37
Daniel A. Steffen <das@users.sourceforge.net>
2009-08-25 02:20:12 +0200

    adjust project name

--- clang.sh
+++ clang.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
 cd $(dirname $0)/tk/macosx &&
-xcodebuild -project Wish.xcode -target tktest -configuration DebugLeaks clean &&
-scan-build -k -V xcodebuild -project Wish.xcode -target tktest -configuration DebugLeaks
+xcodebuild -project Tk.xcode -target tktest -configuration DebugLeaks clean &&
+scan-build -k -V xcodebuild -project Tk.xcode -target tktest -configuration DebugLeaks
