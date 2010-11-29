8779211f70b0985260df74f40bde0fe47652afb5
nijtmans <nijtmans>
2010-11-29 11:01:53 +0000

    Make assembler code compile in Win64 with gcc.

--- tk/win/tkWin32Dll.c
+++ tk/win/tkWin32Dll.c
@@ -90,12 +90,21 @@ DllEntryPoint(
  *	Always TRUE.
  *
  * Side effects:
- *	This might call some sycronization functions, but MSDN documentation
+ *	This might call some synchronization functions, but MSDN documentation
  *	states: "Waiting on synchronization objects in DllMain can cause a
  *	deadlock."
  *
  *----------------------------------------------------------------------
  */
+
+#if defined(HAVE_NO_SEH) && defined(_WIN64)
+/* A little trick to make the assembler code below
+ * compile on Win64 with gcc: It appears that Win64
+ * does not decorate compiled functions with "_"
+ */
+static void _TkFinalize(ClientData clientData) __attribute__((used));
+static void _TkFinalize(ClientData clientData) {TkFinalize(clientData);}
+#endif
 
 BOOL APIENTRY
 DllMain(
